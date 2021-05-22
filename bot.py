449e8e47413f7436b4c2d7d6d89cde75d90ac628
sumanth <sumanth2594@gmail.com>
2021-05-22 15:01:06 +0530

    added return with modmsg var

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ def start(update, context):
 
 # Create the new URL with the refer tag
 def newReferURL(pcode):
-    modmsg = baseURL+pcode+"?tag="+affiliate_tag+msg
+    modmsg = baseURL+pcode+"?tag="+affiliate_tag+pCode
     return modmsg
 
 #Expand shorted URL (amzn.to links) to normal Amazon URL
